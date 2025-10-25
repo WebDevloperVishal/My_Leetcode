835c84187fb0c05a73296b76efc95ef4139b535f
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-25 20:19:21 +0530

    commit

---
+++ src/lib/db.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from "../generated/prisma/index.js"
+
+const globalForPrisma = globalThis;
+
+export const db = globalForPrisma.prisma || new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+});
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
