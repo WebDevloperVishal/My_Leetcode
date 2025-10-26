a13c0353923faebbe26a3f7f3fcd2233f96334da
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-26 15:10:11 +0530

    commit

--- src/controllers/auth.controler.js
+++ src/controllers/auth.controler.js
@@ -1,11 +1,12 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
-import {db} from "../lib/db.js";
+import { db } from "../lib/db.js";
 import { UserRole } from "../generated/prisma/index.js";
 
-export const register = async (req , res)=>{
-    const { email, password, name } = req.body;
+// Register Controller
+export const register = async (req, res) => {
+  const { email, password, name } = req.body;
 
   try {
     const existingUser = await db.user.findUnique({
@@ -57,10 +58,10 @@ export const register = async (req , res)=>{
 }
 
 
-export const login = async (req , res)=>{}
+export const login = async (req, res) => { }
 
 
-export const logout = async (req , res)=>{}
+export const logout = async (req, res) => { }
 
 
-export const checkAuth = async (req , res)=>{}
+export const checkAuth = async (req, res) => { }
