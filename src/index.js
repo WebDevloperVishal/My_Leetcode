2513198ff8641f6b99c360108261b7fe8c419ed2
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-26 15:07:43 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 
-// import authRouter from "./routes/auth.routes.js";
+import authRouter from "./routes/auth.routes.js";
                                                                                                                                                                                                                                                                                                                                                                                        
 dotenv.config();
 
@@ -19,11 +19,11 @@ app.use(morgan("dev"));
 app.get('/', (req, res) => {
     res.json({ 
         success: true ,
-        message: "Welcome to leetcode api "
+        message: "Welcome to leetcode api"
     })
 })
 
-// app.use("api/v1/auth" , authRouter);
+app.use("api/v1/auth" , authRouter);
 
 app.listen(port, () =>{ 
     console.log(`Example app listening at http://localhost:${port}`)
