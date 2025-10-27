00251816a0c495a22d230f37718841d2bbd85231
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-28 00:28:04 +0530

    commit

--- src/routes/problem.routes.js
+++ src/routes/problem.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authenticate, checkAdmin } from "../middlewares/auth.middleware";
+import { authenticate, checkAdmin } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
