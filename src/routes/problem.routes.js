3e8b1b2c3f48b7f489948426bbd6f603cfbff463
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-28 00:51:31 +0530

    commit

--- src/routes/problem.routes.js
+++ src/routes/problem.routes.js
@@ -1,8 +1,25 @@
 import express from "express";
 import { authenticate, checkAdmin } from "../middlewares/auth.middleware.js";
+import {
+    createProblem,
+    deleteProblem,
+    getAllProblem,
+    getAllProblemSolvedByUser,
+    getProblemById,
+    updateProblem
+} from "../controllers/problem.contoler.js";
 
 const router = express.Router();
 
-router.post("/create-problem", authenticate, checkAdmin , createProblem)
+router.post("/create-problem", authenticate, checkAdmin, createProblem);
 
+router.get("/get-all-problems", authenticate, getAllProblem);
+
+router.get("/get-problem/:id", getProblemById);
+
+router.put("/update-problem/:id", authenticate, checkAdmin, updateProblem);
+
+router.delete("/delete-problem/:id", authenticate, checkAdmin, deleteProblem);
+
+router.get("/get-solved-problem", authenticate, getAllProblemSolvedByUser);
 export default router;
