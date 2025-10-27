8070b15ca94c9db7139458e3e1e7369a49da1fb4
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-28 00:43:11 +0530

    commit

---
+++ src/controllers/problem.contoler.js
@@ -0,0 +1,13 @@
+import {db} from"../lib/db.js";
+
+export const createProblem = async (req, res) =>{};
+
+export const getAllProblem = async (req,res) =>{};
+
+export const getProblemById = async (req,res) =>{};
+
+export const updateProblem = async (req,res) =>{};
+
+export const deleteProblem = async (req,res) =>{};
+
+export const getAllProblemSolvedByUser = async (req,res) =>{};
