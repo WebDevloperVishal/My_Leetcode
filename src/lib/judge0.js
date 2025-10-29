f5731e4adea2987ab575c5f3aa3589e3114b864d
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-29 07:54:58 +0530

    commit

--- src/lib/judge0.js
+++ src/lib/judge0.js
@@ -1,15 +1,15 @@
 import axios from "axios"
 
 export function getJudge0LanguageId(language) {
-    const languageMap = {
-        "PYTHON": 71,
-        "JAVASCRIPT": 63,
-        "JAVA": 62,
-        "CPP": 54,
-        "GO": 60,
-    }
+  const languageMap = {
+    "PYTHON": 71,
+    "JAVASCRIPT": 63,
+    "JAVA": 62,
+    "CPP": 54,
+    "GO": 60,
+  }
 
-    return languageMap[language.toUpperCase()]
+  return languageMap[language.toUpperCase()]
 }
 
 export async function submitBatch(submissions) {
@@ -25,4 +25,26 @@ export async function submitBatch(submissions) {
   );
 
   return data;
+}
+
+export async function pollBatchResults(tokens) {
+  while (true) {
+    const { data } = await axios.get(
+      `${process.envv.JUDGEO_API_URL}/submittions/batch`,
+      {
+        params: {
+          tokens: tokens.join(","),
+          base64_encoded: false,
+        }
+      }
+    )
+
+    const results = data.submissions;
+    const isAllDone = results.every(
+      (r) => r.status.id !== 1 && r.status !== 2
+    )
+
+    if (isAllDone) return results;
+    await sleep(1000);
+  }
 }
