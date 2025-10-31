3c61c245584aa4975bac6b28074e271db1f38f66
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-31 07:43:25 +0530

    commit

--- src/controllers/executeCode.controller.js
+++ src/controllers/executeCode.controller.js
@@ -4,7 +4,14 @@ export const executeCode = async(req,res) => {
     const userId = req.user.id;
 
     try {
-        
+        if (
+            !Array.isArray(stdin) ||
+            stdin.length  === 0 ||
+            !Array.isArray(expected_outputs) ||
+            expected_outputs.length !== stdin.length
+        ) {
+            return res.status(400).json({ error: "Invalid or miising test cases"});
+        }
     } catch (error) {
         
     }
