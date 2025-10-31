ab37b431acd98e923b8d4ff02b6d2f457bfff7f2
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-31 07:39:53 +0530

    commit

--- src/controllers/executeCode.controller.js
+++ src/controllers/executeCode.controller.js
@@ -1,3 +1,11 @@
 export const executeCode = async(req,res) => {
-    
+    const {source_code, language_id, stdin, expected_outputs, problemId} = req.body;
+
+    const userId = req.user.id;
+
+    try {
+        
+    } catch (error) {
+        
+    }
 }
