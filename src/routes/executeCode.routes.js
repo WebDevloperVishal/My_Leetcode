ea0101db9738112a2f0131faac4169e33aef29a1
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-31 07:29:14 +0530

    commit

---
+++ src/routes/executeCode.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { authenticate } from "../middlewares/auth.middleware.js";
+
+
+
+const router = express.Router();
+
+router.post("/" , authenticate, executeCode);
+
+export default router;
