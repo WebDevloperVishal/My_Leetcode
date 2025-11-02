f674945517b329437fd2f23bce3cf58b6c2b4b90
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-11-02 14:17:29 +0530

    commit

--- src/routes/executeCode.routes.js
+++ src/routes/executeCode.routes.js
@@ -2,8 +2,6 @@ import express from "express";
 import { authenticate } from "../middlewares/auth.middleware.js";
 import { executeCode } from "../controllers/executeCode.controller.js";
 
-
-
 const router = express.Router();
 
 router.post("/" , authenticate, executeCode);
