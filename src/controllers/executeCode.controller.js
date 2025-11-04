086ca7f20f297c3f0c486fd42010b73cbd364f16
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-11-04 20:37:37 +0530

    commit

--- src/controllers/executeCode.controller.js
+++ src/controllers/executeCode.controller.js
@@ -1,3 +1,5 @@
+import { getLanguageName, pollBatchResults, submitBatch } from "../lib/judge0.js";
+
 export const executeCode = async (req, res) => {
     const { source_code, language_id, stdin, expected_outputs, problemId } = req.body;
 
@@ -20,6 +22,46 @@ export const executeCode = async (req, res) => {
             base64_encoded: false,
             wait: false,
         }))
+
+        const  submitResponse = await submitBatch(submissions);
+
+        const token = submitResponse.map((res)=>res.token);
+
+        const result = await pollBatchResults(token);
+
+        let allPassed = true;
+        const detailedResult = result.map((result , i)=>{
+            const stdout = result.stdin?.trim() || null
+            const expected_output = expected_outputs[i]?.trim();
+            
+            const passed = stdout === expected_output
+
+            if(!passed) allPassed = false;
+
+            return{
+                testCase: i + 1,
+                passed,
+                stdout,
+                expected: expected_output,
+                stdree: result.stdree || null,
+                compile_output: result.compile_output || null,
+                status: result.status.description,
+                memory: result.memory ? `${result.memory} KB` : undefined,
+                time: result.time ? `${result.time} s` : undefined,
+
+            }
+
+        })
+
+        const submission = await db.submission.create({
+            data:{
+                userId,
+                problemId,
+                source_Code:source_code,
+                langage:getLanguageName(language_id)
+            }
+        })
+
     } catch (error) {
 
     }
