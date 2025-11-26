5754704cb343d882d50bf61f428bbe70df187592
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-11-26 07:02:06 +0530

    commit

--- src/controllers/problem.contoler.js
+++ src/controllers/problem.contoler.js
@@ -152,67 +152,66 @@ export const updateProblem = async (req, res) => {
     }
 
     if (req.user.role !== "ADMIN") {
-      return res.status(404).json({ error : "Only Admin can update problems"});
+      return res.status(404).json({ error: "Only Admin can update problems" });
     }
 
 
     for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
       const languageId = getJudge0LanguageId(language);
-      if(!languageId) { return res.status(400). json({ error: "Unsupporetd language: ${language}"});
-    }
+      if (!languageId) {
+        return res.status(400).json({ error: "Unsupporetd language: ${language}" });
+      }
 
-    const submissions = testCases.map(({input, output}) => ({
-      source_code: solutionCode,
-      languageId: languageId,
-      stdin: input,
-      expected_output: output,
-    }));
+      const submissions = testCases.map(({ input, output }) => ({
+        source_code: solutionCode,
+        languageId: languageId,
+        stdin: input,
+        expected_output: output,
+      }));
 
-    console.log("submissions:", submissions)
+      console.log("submissions:", submissions)
 
-    const submissionResults = await submitBatch(submissions);
+      const submissionResults = await submitBatch(submissions);
 
-    const tokens = submissionResults.map((res) => res.tokens);
-    
-    const results = await pollBatchResults(tokens);
+      const tokens = submissionResults.map((res) => res.tokens);
 
-    for (let i = 0; i< results.length, i++) {
-      const result = results[i];
-      if (result.status.id !== 3 ){
-        return res.status(400).json({
-          error: `Validation failed for ${language} on input: ${submissions[i].stdin}`,
-          details: result,
-        });
+      const results = await pollBatchResults(tokens);
+
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+        if (result.status.id !== 3) {
+          return res.status(400).json({
+            error: `Validation failed for ${language} on input: ${submissions[i].stdin}`,
+            details: result,
+          });
+        }
       }
     }
-  }
 
-  const updateProblem = await db.problem.update({
-    where: {id},
-    data: {
-      title,
-      description,
-      difficulty,
-      tags,
-      examples,
-      constraints,
-      testCases,
-      codeSnippets,
-      referenceSolutions
-    },
-  });
+    const updateProblem = await db.problem.update({
+      where: { id },
+      data: {
+        title,
+        description,
+        difficulty,
+        tags,
+        examples,
+        constraints,
+        testCases,
+        codeSnippets,
+        referenceSolutions
+      },
+    });
 
-  res.status(200).json({
-    success: true,
-    message: "Problem upated successfully",
-    problem: updateProblem,
-  })
-
-
+    res.status(200).json({
+      success: true,
+      message: "Problem upated successfully",
+      problem: updateProblem,
+    })
 
   } catch (error) {
     console.log("Enter creating problem:", error);
-      res.status(500).json({ error: "Failed to update problem"})
+    res.status(500).json({ error: "Failed to update problem" })
   }
 
 };
