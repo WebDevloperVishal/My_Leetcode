22646674633d40315570e887da3ab25b18179863
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-11-27 21:33:01 +0530

    commit

--- src/controllers/submission.controller.js
+++ src/controllers/submission.controller.js
@@ -1,6 +1,8 @@
 import { db } from "../lib/db.js";
 
-export const getAllSubmissions = async (req, res) => {};
+export const getAllSubmissions = async (req, res) => {
+    
+};
 
 export const getAllSubmissionsFroProblem = async (req,res) => {};
 
