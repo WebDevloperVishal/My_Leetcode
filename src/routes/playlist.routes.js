b5fc084c3c8681fcc9fe886bd49a0312a1e78050
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-11-28 07:59:11 +0530

    commit

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { authenticate } from "../middlewares/auth.middleware.js";
 import { addProblemToPlayList, 
-    createPlayList, deletePlayList, getPlayAllListDetails, getPlayListDetails, removeProblemFromPlayList } from "../controllers/playlist.controller.js";
+    createPlayList, 
+    deletePlayList, getPlayAllListDetails, getPlayListDetails, removeProblemFromPlayList } from "../controllers/playlist.controller.js";
 
 
 const router = express.Router();
