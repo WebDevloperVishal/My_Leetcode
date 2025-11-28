629f23bf50b77546939598cf4d501ba2479bcdd7
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-11-28 07:29:21 +0530

    commit

--- src/controllers/submission.controller.js
+++ src/controllers/submission.controller.js
@@ -18,7 +18,21 @@ export const getAllSubmissions = async (req, res) => {
 
 export const getAllSubmissionsFroProblem = async (req, res) => {
     try {
-        
+        const userId = req.user.id;
+        const problemId = req.params.problemId;
+        const submissions = await db.submission.findMany({
+            where:{
+                userId: userId,
+                problemId: problemId
+            },
+        })
+
+
+        res.status(200).json({
+            success: true,
+            message: "Submissions fetch successfully", 
+            submissions,
+        })
     } catch (error) {
         console.log("Fetch Submittions Error", error);
         res.status(500).json({ error: "Failed to fetch submissions"})
