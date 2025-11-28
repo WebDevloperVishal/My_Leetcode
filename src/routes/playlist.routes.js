94aa880e3ea6ca2114c0b56417cef4f0d01f18ec
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-11-28 07:58:38 +0530

    commit

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,8 +1,21 @@
 import express from "express";
+import { authenticate } from "../middlewares/auth.middleware.js";
+import { addProblemToPlayList, createPlayList, deletePlayList, getPlayAllListDetails, getPlayListDetails, removeProblemFromPlayList } from "../controllers/playlist.controller.js";
 
 
 const router = express.Router();
 
+router.get("/", authenticate, getPlayAllListDetails);
+
+router.get("/:playlistid", authenticate, getPlayListDetails);
+
+router.post("/create-playlist", authenticate, createPlayList);
+
+router.post("/:playlistId/add-problem", authenticate, addProblemToPlayList);
+
+router.delete("/:playlistId", authenticate, deletePlayList);
+
+router.delete("/:playlistId/remove-problem", authenticate, removeProblemFromPlayList)
 
 
 export default router;
