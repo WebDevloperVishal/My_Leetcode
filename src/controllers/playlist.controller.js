0597082587f097f485169461da3302eb70355a06
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-11-28 07:51:20 +0530

    commit

---
+++ src/controllers/playlist.controller.js
@@ -0,0 +1,14 @@
+import { db } from "../lib/db.js";
+
+
+export const createPlayList = async (req, res) => {};
+
+export const getPlayAllListDetails = async (req,res) => {};
+
+export const getPlayListDetails = async (req,res) => {};
+
+export const addProblemToPlayList = async (req, res) => {};
+
+export const deletePlayList = async (req,res) => {};
+
+export const removeProblemFromPlayList = async (req,res) => {};
