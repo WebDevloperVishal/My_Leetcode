3d9f8fe31c085383f6711a4c100e858323031c05
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-11-29 20:17:22 +0530

    commit

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,17 +3,19 @@ import { db } from "../lib/db.js";
 export const createPlayList = async (req, res) => { 
     try {
         const { name, description } = req.body;
-        const userId= req.user.id;
+        const userId = req.user.id;
 
-        const PlayList = await db.playlist.create({
+        const playList = await db.playlist.create({
             data: {
                 name,
                 description,
                 userId,
             },
         });
+        res.status(200).json({ success: true, message: "Playlist create successfully", playList})
     } catch (error) {
-        
+        console.log("Error creating playlist", error);
+        res.status(500).json({ error: "Failed to create playlist"});
     }
 };
 
