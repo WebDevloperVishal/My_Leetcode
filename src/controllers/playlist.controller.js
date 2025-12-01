bcc933992aa6ff5ae92f25b0b859e67667546741
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-12-01 20:07:44 +0530

    commit

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -98,7 +98,7 @@ export const addProblemToPlayList = async (req, res) => {
 };
 
 export const deletePlayList = async (req, res) => {
-const { playlistId } = req.params;
+  const { playlistId } = req.params;
 
   try {
     const deletedPlaylist = await db.playlist.delete({
@@ -119,6 +119,31 @@ const { playlistId } = req.params;
 
 };
 
-export const removeProblemFromPlayList = async (req, res) => { 
-  
+export const removeProblemFromPlayList = async (req, res) => {
+  const { playlistId } = req.params;
+  const { problemIds } = req.body;
+
+  try {
+    if (!Array.isArray(problemIds) || problemIds.length === 0) {
+      return res.status(400).json({ error: "Invalid or missing problemIds" })
+    }
+
+    const deletedProblem = await db.problemInPlaylist.deleteMany({
+      where: {
+        playlistId,
+        problemId: {
+          id: problemIds,
+        }
+      }
+    })
+
+    res.status(200).json({
+      success: true,
+      message: "Problem removed from playlist successfully",
+      deletedProblem,
+    })
+  } catch (error) {
+    console.error('Error removing problem from playlist:', error.message);
+    res.status(500).json({ error: 'Failed to remove problem from playlist' });
+  }
 };
